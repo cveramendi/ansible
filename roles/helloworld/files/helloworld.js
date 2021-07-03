b3ae92dda034b977c746e33910c915f9824cad33
christiand <neosoul_jb@hotmail.com>
2021-07-03 00:40:22 -0500

    v3

--- roles/helloworld/files/helloworld.js
+++ roles/helloworld/files/helloworld.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    //response.end('Hello World - CHANGED BY ANSIBLE PLAYBOOK\n')
-   response.end('Hello World - CHANGED BY ANSIBLE PULL v2\n')
+   response.end('Hello World - CHANGED BY ANSIBLE PULL v3\n')
 }).listen(3000)
 
 // Console will print the message
